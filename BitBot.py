b9ae875f94d776fc40821ad448f49b7ec7d10295
Minecon724 <39800107+Minecon724@users.noreply.github.com>
2018-09-01 20:16:50 +0200

    Update BitBot.py

--- BitBot.py
+++ BitBot.py
@@ -4,7 +4,7 @@ import os
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
-from discord import opus
+import opuslib
 import asyncio
 import random
 import psutil
