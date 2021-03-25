341b83a3eadf07c4e3cac1fad6804a381b9813e5
Minecon724 <39800107+Minecon724@users.noreply.github.com>
2021-03-25 16:28:28 +0100

    Update BitBot.py

--- BitBot.py
+++ BitBot.py
@@ -500,21 +500,21 @@ async def Konfiguruj(ctx, co=None, *, wartosc=None):
         if wartosc == None:
             embed = discord.Embed(title="Konfiguruj bota:")
             if co == "joindm":
-                embed.add_field(name="Wartość to:", value="{}".format(Konfiguracje.JoinDM(ctx.message.guild.id)), inline=True)
+                embed.add_field(name="Wartość to:", value="{}".format(Konfiguracje.JoinDM(str(ctx.message.guild.id))), inline=True)
             elif co == "removedm":
-                embed.add_field(name="Wartość to:", value="{}".format(Konfiguracje.RemoveDM(ctx.message.guild.id)), inline=True)
+                embed.add_field(name="Wartość to:", value="{}".format(Konfiguracje.RemoveDM(str(ctx.message.guild.id))), inline=True)
             elif co == "lang":
-                embed.add_field(name="Wartość to:", value="{}".format(Konfiguracje.RemoveDM(ctx.message.guild.id)), inline=True)
+                embed.add_field(name="Wartość to:", value="{}".format(Konfiguracje.RemoveDM(str(ctx.message.guild.id))), inline=True)
             embed.set_footer(text="Jeżeli chcesz ustawić wartość, użyj *{}Konfiguruj {} <wartość>*.".format(prefix, co))
             await ctx.reply(embed=embed)
             return
         l = str(wartosc).lower()
         if co == "joindm":
-            Konfiguracje.UstawJoinDM(ctx.message.guild.id, wartosc)
+            Konfiguracje.UstawJoinDM(str(ctx.message.guild.id), wartosc)
         elif co == "removedm":
-            Konfiguracje.UstawRemoveDM(ctx.message.guild.id, wartosc)
+            Konfiguracje.UstawRemoveDM(str(ctx.message.guild.id), wartosc)
         elif co == "lang":
-            Konfiguracje.UstawJezyk(ctx.message.guild.id, wartosc)
+            Konfiguracje.UstawJezyk(str(ctx.message.guild.id), wartosc)
             if not wartosc in jezyki:
                 ctx.message.reply("Dostepne jezyki: " + ', '.join(jezyki))
                 return
