c4dedd2a9f7580415b4876d6e6bb5094bcdfcdc7
Minecon724 <@>
2021-09-28 18:24:16 +0200

    asd

--- src/main/java/pl/minecon724/bitbot/BitBot.java
+++ src/main/java/pl/minecon724/bitbot/BitBot.java
@@ -1,7 +1,12 @@
 package pl.minecon724.bitbot;
 
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class BitBot {
-	public void main(String[] args) {
-		
+	public void main(String[] args) throws LoginException {
+		JDA jda = JDABuilder.createDefault(args[0]).build();
 	}
 }
